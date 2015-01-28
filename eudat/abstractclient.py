b6318d455af9f659fba8c9f8b99a2aa13a6aea7b
muccix <muccix@gmail.com>
2015-01-28 10:53:17 +0100

    Implemented find datasets by metadata

--- eudat/abstractclient.py
+++ eudat/abstractclient.py
@@ -7,6 +7,7 @@ Client abstract class
 __author__ = 'Roberto Mucci (r.mucci@cineca.it)'
 
 from abc import ABCMeta, abstractmethod
+import find.getdatasetinfo as datasetinfo
 
 
 class AbstractClient(object):
@@ -17,24 +18,6 @@ class AbstractClient(object):
         """ Store the object containing the information needed for
         the authentication """
         pass
-
-        """# Need to find a smart method to check if auth is a list or an object
-        if len(auth) < 2:
-            print "/'auth/' object must contain at least 2 values"
-
-        self.auth = auth
-        self.session = http_session
-        if self.auth[0]:
-            print "user name set as {0}".format(self.auth[0])
-        if self.auth[1]:
-            print "password set as ************".format(self.auth[1])
-
-        if len(self.auth) > 2:
-            if self.auth[2]:
-                print "cert file set as {0}".format(self.auth[2])
-            if self.auth[3]:
-                print "key file set as {0}".format(self.auth[3])
-                """
 
     @abstractmethod
     def login(self):
@@ -47,3 +30,26 @@ class AbstractClient(object):
     @abstractmethod
     def get(self):
         pass
+
+    def find_by_metadata(self, ckan_url='eudat-b1.dkrz.de', community='',
+                         pattern=[], ckan_limit=1000):
+        """
+        Retrieve datasets information by given search criteria.
+        Returns a list of datasets (each dataset is a list of dictionary
+        composed by key and value).
+
+        ckan_url : string
+            CKAN portal address, to which search requests are submitted
+            (default is eudat-b1.dkrz.de).
+        community : string
+            Community where you want to search in.
+        pattern : list
+            CKAN search pattern, i.e. (a list of) field:value terms.
+        ckan_limit : int
+            Limit of listed datasets (default is 1000).
+        """
+        results = datasetinfo.get_dataset_info(ckan_url=ckan_url,
+                                               community=community,
+                                               pattern=pattern,
+                                               ckan_limit=ckan_limit)
+        return results
