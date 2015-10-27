70706a0d15a6ccef4342637a5002dc2b085cac07
muccix <muccix@gmail.com>
2015-10-27 17:20:51 +0100

    changed organization of files

---
+++ eudat/b2find.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+"""
+Retrieve dataset info by given search criteria using CKAN portal
+"""
+
+__author__ = 'Roberto Mucci (r.mucci@cineca.it)'
+
+
+import json
+import requests
+
+
+def get_dataset_source(ckan_url='eudat-b1.dkrz.de', community='', pattern=[],
+                     ckan_limit=1000):
+    """
+    Retrieve datasets source by given search criteria using CKAN portal.
+
+    :param ckan_url: CKAN portal address, to which search requests are submitted
+        (default is eudat-b1.dkrz.de).
+    :param community: Community where you want to search in.
+    :param pattern: CKAN search pattern, i.e. (a list of) field:value terms.
+    :param ckan_limit: Limit of listed datasets (default is 1000).
+    :return: list of datasets source (each source should be physical URL to the
+    data object).
+    """
+
+    if (not pattern) and (not community):
+        print "[ERROR] Need at least a community or a search pattern as " \
+              "argument!"
+        return
+
+    ckan_pattern = ''
+    sand = ''
+    pattern = ' AND '.join(pattern)
+    if community:
+        ckan_pattern += "groups:%s" % community
+        sand = " AND "
+    if pattern:
+        ckan_pattern += sand + pattern
+
+    print "Search in %s for pattern %s\n....." % (ckan_url, ckan_pattern)
+    answer = _action(ckan_url, {"q": ckan_pattern, "rows": ckan_limit,
+                               "start": 0})
+
+    if answer is None:
+        return answer
+
+    countURL = 0
+    results = []
+    for ds in answer['result']['results']:
+            results.append(ds['url'])
+            countURL += 1
+
+    print "Found %d Sources\n" % (countURL)
+    return results
+
+
+def get_dataset_info(ckan_url='eudat-b1.dkrz.de', community='', pattern=[],
+                     ckan_limit=1000):
+    """
+    Retrieve datasets info by given search criteria using CKAN portal.
+
+    :param ckan_url: CKAN portal address, to which search requests are submitted
+        (default is eudat-b1.dkrz.de).
+    :param community: Community where you want to search in.
+    :param pattern: CKAN search pattern, i.e. (a list of) field:value terms.
+    :param ckan_limit: Limit of listed datasets (default is 1000).
+    :return: list of datasets (each dataset is a list of dictionary
+    composed by key and value) considering only the datasets containing a pid
+    value.
+    """
+    if (not pattern) and (not community):
+        print "[ERROR] Need at least a community or a search pattern as " \
+              "argument!"
+        return
+
+    ckan_pattern = ''
+    sand = ''
+    pattern = ' AND '.join(pattern)
+    if community:
+        ckan_pattern += "groups:%s" % community
+        sand = " AND "
+    if pattern:
+        ckan_pattern += sand + pattern
+
+    print "Search in %s for pattern %s\n....." % (ckan_url, ckan_pattern)
+    answer = _action(ckan_url, {"q": ckan_pattern, "rows": ckan_limit,
+                               "start": 0})
+
+    if answer is None:
+        return answer
+
+    countPID = 0
+    results = []
+    for ds in answer['result']['results']:
+            for extra in ds['extras']:
+                if extra['key'] == 'PID':
+                    # add dataset to list
+                    results.append(ds['extras'])
+                    countPID += 1
+                    break
+
+    print "Found %d PIDs\n" % (countPID)
+    return results
+
+
+def _action(host, data={}):
+    return __action_api(host, 'package_search', data)
+
+
+def __action_api(host, action, data_dict):
+    # Make the HTTP request for data set generation.
+    action_url = "http://{host}/api/3/action/{action}".format(host=host,
+                                                              action=action)
+
+    try:
+        response = requests.get(action_url, params=data_dict)
+    except requests.exceptions.RequestException as e:
+        print e.message
+        return
+    except requests.exceptions.HTTPError as e:
+        print e
+        return
+    if response.status_code != 200:
+        print "Error code {0}. The server {1} couldn't fulfill the action {2}.\n".format(response.status_code, host, action)
+        return
+    out = json.loads(response.text)
+    return out
+
+
+def main():
+    """ Main function to test the script """
+    #get_dataset_info(pattern=['tags:MPIOM'])
+    get_dataset_info(community='aleph')
+    get_dataset_source(community='aleph')
+    get_dataset_source(pattern=['tags:climate'])
+
+
+if __name__ == '__main__':
+    main()
---
+++ eudat/getpidinfo.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+"""
+Retrieve pid information accessing the handle resolution system using HTTP
+"""
+
+__author__ = 'Roberto Mucci (r.mucci@cineca.it)'
+
+
+import json
+import urllib2
+
+
+def get_pid_info(pid, handle_url='hdl.handle.net'):
+    """
+    Resolve pid information accessing the handle resolution system provider
+    using HTTP REST API.
+
+    :param pid: PID that has to be resolved
+    :param handle_url: Handle system provider address
+     (default is hdl.handle.net).
+    :return: a list of dictionary containing PID information.
+    """
+    if not pid:
+        print "[ERROR] PID is needed to submit the request.. "
+        return
+
+    print "Search in\t%s\nfor pid\t%s\n....." % (handle_url, pid)
+    answer = __action_api(handle_url, pid)
+
+    if answer == None:
+        return answer
+
+    values = answer['values']
+    return values
+
+
+def __action_api(host, pid):
+    """ Make the HTTP request."""
+    action_url = "http://{host}/api/handles/{pid}".format(host=host, pid=pid)
+
+    try:
+        request = urllib2.Request(action_url)
+        response = urllib2.urlopen(request)
+    except urllib2.HTTPError as e:
+        print "\t\tError code %s : The server %s responded with an error" \
+              % (e.code, host)
+        if e.code == 500:
+            print '\t\tError. Something unexpected went wrong during handle ' \
+                  'resolution. (HTTP 500 Internal Server Error)'
+            return
+        elif e.code == 404:
+            print '\t\tHandle Not Found. (HTTP 404 Not Found)'
+            return
+
+    except urllib2.URLError as e:
+        print 'urllib2.URLError: {0}'.format(e.reason)
+        return
+    else:
+        out = json.loads(response.read())
+        if out['responseCode'] == 200:
+            print 'Values Not Found. The handle exists but has no values ' \
+                  '(or no values according to the types and indices specified)'
+
+        assert response.code >= 200
+        return out
+
+
+def main():
+    """ Main function to test the script """
+    get_pid_info(pid='11100/0beb6af8-cbe5-11e3-a9da-e41f13eb41b2')
+
+
+if __name__ == '__main__':
+    main()
