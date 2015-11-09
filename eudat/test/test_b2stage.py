28f055043725c83dffb236ced929fce74ed9b505
muccix <muccix@gmail.com>
2015-11-09 11:01:41 +0100

    Use json file to set credentials and path used in the test

--- eudat/test/test_b2stage.py
+++ eudat/test/test_b2stage.py
@@ -1,48 +1,49 @@
 #!/usr/bin/env python
 
 """
-Test  b2stage.py
+Test b2stage.py
 """
 
 __author__ = 'rmucci00'
 
 import os
+import json
 from nose.tools import assert_equals
 from eudat import b2stage
+
+# Credentials and other necessary values that should not be public:
+RESOURCES_FILE = os.path.join(os.getcwd(),
+                                 'eudat/test/b2stage_credentials_test_template.json')
+testvalues = json.load(open(RESOURCES_FILE))
 
 
 def test_globus_login():
     """ B2STAGE: Globus online login """
-    resource_file = os.path.join(os.getcwd(),
-                                 'eudat/test/b2stage_credentials_test_template.json')
-    globus = b2stage.ClientGlobus(resource_file_path=resource_file);
+    globus = b2stage.ClientGlobus(resource_file_path=RESOURCES_FILE);
 
     assert globus.api is not None
 
 
 def test_globus_endpoint_activation():
     """ B2STAGE: Globus online GridFTP endpoint activation """
-    resource_file = os.path.join(os.getcwd(),
-                                 'eudat/test/b2stage_credentials_test_template.json')
-    globus = b2stage.ClientGlobus(resource_file_path=resource_file);
-    result = globus.endpoint_activation('cineca#GALILEO')
+
+    globus = b2stage.ClientGlobus(resource_file_path=RESOURCES_FILE);
+    result = globus.endpoint_activation(testvalues['dst_endpoint'])
     assert_equals(result, True)
 
 
-
 def test_globus_transfer():
-    """ B2STAGE: Globus online data transfer """
+    """ B2STAGE: Globus online folder transfer """
 
-    resource_file = os.path.join(os.getcwd(),
-                                 'eudat/test/b2stage_credentials_test_template.json')
-    globus = b2stage.ClientGlobus(resource_file_path=resource_file);
-    task_id = globus.transfer('cineca#DataRepository','cineca#PICO',
-                              '/CINECA01/home/cin_staff/rmucci00/DSI_Test/',
-                              '/pico/home/userinternal/rmucci00/DSI_Test/')
+    globus = b2stage.ClientGlobus(resource_file_path=RESOURCES_FILE);
+    # src_item must be a folder, since recursive is set to True
+    task_id = globus.transfer(testvalues['src_endpoint'],
+                              testvalues['dst_endpoint'],
+                              testvalues['src_item'],
+                              testvalues['dst_dir'], recursive=True)
 
     assert task_id is not None
 
-    #globus.display_task(task_id, False)
     status = globus.wait_for_task(task_id, timeout=60, poll_interval=10)
     assert_equals(status, "SUCCEEDED")
-    globus.display_task(task_id)
+    globus.display_successful_transfer(task_id)
